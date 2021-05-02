5ac0b1075cf8b37bb4c55d16ba044fd7fc6beb10
GitHub Action <action@github.com>
2021-05-02 10:20:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/niradler/checkovjetbrainsidea/MyBundle.kt
+++ src/main/kotlin/com/github/niradler/checkovjetbrainsidea/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.niradler.checkovjetbrainsidea
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/niradler/checkovjetbrainsidea/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.niradler.checkovjetbrainsidea.services
+
+import com.github.niradler.checkovjetbrainsidea.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
