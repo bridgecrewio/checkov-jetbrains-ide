3275840d2c16982d8038e00a3ee6a62205665e03
Nir Adler <nir@bridgecrew.io>
2021-05-11 11:32:05 +0300

    fix conf

--- src/main/kotlin/com/bridgecrew/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/bridgecrew/listeners/MyProjectManagerListener.kt
@@ -1,12 +1,6 @@
-<<<<<<< HEAD:src/main/kotlin/com/github/bridgecrewio/checkov/listeners/MyProjectManagerListener.kt
 package com.github.bridgecrewio.checkov.listeners
 
 import com.github.bridgecrewio.checkov.services.MyProjectService
-=======
-package com.bridgecrew.listeners
-
-import com.bridgecrew.services.MyProjectService
->>>>>>> 5069401bc9c3f0dba82fdbef6c97312569d32725:src/main/kotlin/com/bridgecrew/listeners/MyProjectManagerListener.kt
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
