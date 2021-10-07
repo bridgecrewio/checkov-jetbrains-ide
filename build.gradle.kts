4f17dbd6b299d3ddf4c22e59efd4497d0c92abfe
yuval yacoby <yacoby133@gmail.com>
2021-10-07 11:17:21 +0300

    update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,10 @@ dependencies {
     implementation("com.beust:klaxon:5.5")
 }
 
+plugins {
+    "com.google.code.gson"
+}
+
 // Configure gradle-intellij-plugin plugin.
 // Read more: https://github.com/JetBrains/gradle-intellij-plugin
 intellij {
