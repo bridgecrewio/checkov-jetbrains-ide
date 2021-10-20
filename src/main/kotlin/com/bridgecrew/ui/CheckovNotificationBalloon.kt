dba0db8e21d1e90df8fa467e9ff835bd8ed528b2
orhovy <yorhov@paloaltonetworks.com>
2021-10-20 17:03:01 +0300

    reverted changed

--- src/main/kotlin/com/bridgecrew/ui/CheckovNotificationBalloon.kt
+++ src/main/kotlin/com/bridgecrew/ui/CheckovNotificationBalloon.kt
@@ -13,36 +13,15 @@ import com.intellij.openapi.project.Project
 class CheckovNotificationBalloon {
 
     companion object {
-<<<<<<< Updated upstream
-=======
-        val title = "Checkov"
->>>>>>> Stashed changes
         private val groupNeedAction = "CheckovNeedAction"
         private val GROUP = NotificationGroup(groupNeedAction, NotificationDisplayType.STICKY_BALLOON)
 
         fun showError(project: Project, failureNumber: Int) {
-<<<<<<< Updated upstream
                 val notification = GROUP.createNotification(
                     "Checkov has detected $failureNumber configuration error(s) in your project. Check out the tool window to analyze your code ",
                     NotificationType.WARNING
                 )
                 notification.notify(project)
-=======
-            if (!CheckovToolWindowPanel(project).isVisible) {
-                val notification = GROUP.createNotification(
-                    "Checkov has finished ",
-                    NotificationType.WARNING
-                ).addAction(
-                    NotificationAction.createSimpleExpiring("Go to results\u2026") {
-                        CheckovToolWindowPanel(project).isVisible = true
-
-                        println("before direct")
-                    }
-                )
-
-                notification.notify(project)
-            }
->>>>>>> Stashed changes
         }
     }
 }
