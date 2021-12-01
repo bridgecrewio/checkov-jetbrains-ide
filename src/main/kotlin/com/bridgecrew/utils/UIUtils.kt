5d03facb8a0b8a8da86d72252cd2c6c3c692ec05
orhovy <yorhov@paloaltonetworks.com>
2021-12-01 16:26:12 +0200

    added custom policy

--- src/main/kotlin/com/bridgecrew/utils/UIUtils.kt
+++ src/main/kotlin/com/bridgecrew/utils/UIUtils.kt
@@ -1,9 +1,5 @@
 package com.bridgecrew.utils
 
-import com.bridgecrew.CheckovResult
-import com.bridgecrew.services.CheckovScanService
-import com.intellij.openapi.components.service
-import com.intellij.openapi.project.Project
 import com.intellij.ui.components.labels.LinkLabel
 import com.intellij.uiDesigner.core.GridConstraints
 import java.awt.Desktop
@@ -13,18 +9,23 @@ import java.net.URISyntaxException
 import javax.swing.JLabel
 import javax.swing.JTextPane
 
-fun urlLink(title:String, url: String): JLabel{
-    var link = JLabel()
-
-    if (isUrl(url)) {
-        link = LinkLabel.create(title) {
+fun urlLink(title:String, guideline: String): JLabel{
+    val guidelineLabel = JLabel()
+    val titleName = JLabel(title)
+    guidelineLabel.add(titleName)
+    if (isUrl(guideline)) {
+        val url = LinkLabel.create(guideline) {
             try {
-                Desktop.getDesktop().browse(URI(url))
+                Desktop.getDesktop().browse(URI(guideline))
             } catch (ex: URISyntaxException) {
             }
         }
+        guidelineLabel.add(url)
+    } else {
+        val customGuideline = JLabel(guideline)
+        guidelineLabel.add(customGuideline)
     }
-    return link
+    return guidelineLabel
 }
 
 fun createTitle(title: String, font: Int, size: Int): JTextPane {
