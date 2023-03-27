23ce73021ac3e12659b6cd563c4bbc25b157c3f2
mshavit <mshavit@@paloaltonetworks.com>
2023-03-27 14:23:33 +0300

    focus on the file in editor on selecting in tree + in suppression and fix

--- src/main/kotlin/com/bridgecrew/utils/fileUtils.kt
+++ src/main/kotlin/com/bridgecrew/utils/fileUtils.kt
@@ -5,80 +5,25 @@ import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
-import org.apache.commons.io.FileUtils
 import org.apache.commons.io.FilenameUtils
 import org.jetbrains.rpc.LOG
-import java.io.File
 import java.net.URL
 
-fun navigateToFile(fileToNavigate: PsiFile) {
+fun navigateToFile(project: Project, virtualFile: VirtualFile, startOffset: Int = 0) {
     PsiNavigationSupport.getInstance().createNavigatable(
-        fileToNavigate.project,
-        fileToNavigate.virtualFile,
-        0
+            project,
+            virtualFile,
+            startOffset
     ).navigate(false)
 }
 
-fun getPsFileByPath(path: String, project: Project): PsiFile? {
-    val absolutePath = project.getBasePath() + if (path.startsWith("/")) path else "/$path"
-    val virtualFile = LocalFileSystem.getInstance().findFileByPath(absolutePath)
-    if (virtualFile != null) {
-        return PsiManager.getInstance(project).findFile(virtualFile)
-    }
-    return null
-}
-
-fun getOffsetHighlighByLines(range:List<Int>, project: Project): Pair<Int,Int>{
-    val startLine: Int = range.getOrElse(0, { 1 }) - 1
-    val editor = FileEditorManager.getInstance(project).selectedTextEditor
-    val document = editor?.getDocument();
-    var startOffset = document?.getLineStartOffset(startLine)
-    var endOffset = document?.getLineEndOffset(startLine)
-    if (endOffset == null) {
-        endOffset = 0
-    }
-    if(startOffset == null) {
-        startOffset = 0
-    }
-    return Pair(startOffset, endOffset)
-}
-
-fun getOffsetReplaceByLines(range:List<Int>, project: Project): Pair<Int,Int>{
-    val startLine: Int = range.getOrElse(0, { 1 }) - 1
-    val endLine: Int = range.getOrElse(1, { 1 }) - 1
-    val editor = FileEditorManager.getInstance(project).selectedTextEditor
-    val document = editor?.getDocument();
-    var startOffset = document?.getLineStartOffset(startLine)
-    var endOffset = document?.getLineEndOffset(endLine)
-    if (endOffset == null) {
-        endOffset = 0
-    }
-    if(startOffset == null) {
-        startOffset = 0
-    }
-    return Pair(startOffset, endOffset)
-}
-
-fun updateFile(replaceString: String, project: Project, start:Int, end: Int){
-    val editor = FileEditorManager.getInstance(project).selectedTextEditor
-    val document = editor?.getDocument();
-    WriteCommandAction.runWriteCommandAction(
-        project
-    ) { document?.replaceString(start, end, replaceString) }
-    FileDocumentManager.getInstance().saveDocument(document!!)
-}
-
-
-fun normalizeFilePathToAbsolute(fileName: String, projectBasePath: String, fileRelativePath: String = ""): String {
-    return if (fileName.startsWith(DOCKER_MOUNT_DIR)) {
-        val relativePathParts = mutableListOf(fileRelativePath.split("/")).removeLast()
-        val relativeFileDirectory = relativePathParts.joinToString("/")
-        "$projectBasePath${relativeFileDirectory}"
-    } else {
-        fileName
-    }
+fun navigateToFile(project: Project, filePath: String, startOffset: Int = 0) {
+    val virtualFile: VirtualFile = LocalFileSystem.getInstance().findFileByPath(filePath)
+            ?: return
+    navigateToFile(project, virtualFile, startOffset)
 }
 
 /**
