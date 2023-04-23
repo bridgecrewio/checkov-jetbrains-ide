e5dc6d39a7de6ca2cb4f1bb065d9ac6ab9125f25
Nir Adler <niradler@users.noreply.github.com>
2023-04-23 16:54:08 +0300

    Add CA cert support (#210)
    * add ca support
    
    * pull
    
    * pull
    
    ---------
    
    Co-authored-by: Nir Adler <nadler@paloaltonetworks.com>

--- src/main/kotlin/com/bridgecrew/services/checkovScanCommandsService/DockerCheckovScanCommandsService.kt
+++ src/main/kotlin/com/bridgecrew/services/checkovScanCommandsService/DockerCheckovScanCommandsService.kt
@@ -13,12 +13,18 @@ class DockerCheckovScanCommandsService(project: Project) : CheckovScanCommandsSe
         val pluginVersion =
                 PluginManagerCore.getPlugin(PluginId.getId("com.github.bridgecrewio.checkov"))?.version ?: "UNKNOWN"
 
-        val volumeDir = "${FilenameUtils.separatorsToUnix(project.basePath!!)}:/${volumeDirectory}"
         val dockerCommand = arrayListOf("docker", "run", "--rm", "-a", "stdout", "-a", "stderr", "--env", "BC_SOURCE=jetbrains", "--env", "BC_SOURCE_VERSION=$pluginVersion", "--env", "LOG_LEVEL=DEBUG")
         val prismaUrl = settings?.prismaURL
+        val certPath = settings?.certificate
         if (!prismaUrl.isNullOrEmpty()) {
             dockerCommand.addAll(arrayListOf("--env", "PRISMA_API_URL=${prismaUrl}"))
         }
+
+        if (certPath?.isNotEmpty() == true) {
+            var volumeCaFile = "$certPath:/usr/lib/ssl/cert.pem"
+            dockerCommand.addAll(arrayListOf("--volume", volumeCaFile, "--env", "SSL_CERT_FILE=/usr/lib/ssl/cert.pem", "--env", "REQUESTS_CA_BUNDLE=/usr/lib/ssl/cert.pem"))
+        }
+        val volumeDir = "${FilenameUtils.separatorsToUnix(project.basePath!!)}:/${volumeDirectory}"
         dockerCommand.addAll(arrayListOf("--volume", volumeDir, image))
         return dockerCommand
 
