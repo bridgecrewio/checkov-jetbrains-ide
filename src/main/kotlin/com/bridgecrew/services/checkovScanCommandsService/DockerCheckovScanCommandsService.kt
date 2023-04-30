155afd5eddbb11b381b4f9248f3820065c2a65ee
mayshavit <mayshavit1234@gmail.com>
2023-04-30 16:01:08 +0300

    fix docker scanning (#219)
    * fix docker mapping + improve logs + fix checkov logos and texts to Prisma Cloud
    
    * change name to Prisma Cloud
    
    ---------
    
    Co-authored-by: mshavit <mshavit@@paloaltonetworks.com>

--- src/main/kotlin/com/bridgecrew/services/checkovScanCommandsService/DockerCheckovScanCommandsService.kt
+++ src/main/kotlin/com/bridgecrew/services/checkovScanCommandsService/DockerCheckovScanCommandsService.kt
@@ -8,8 +8,9 @@ import org.apache.commons.io.FilenameUtils
 class DockerCheckovScanCommandsService(project: Project) : CheckovScanCommandsService(project) {
 
     private val image = "bridgecrew/checkov"
-    private val volumeDirectory = "checkovScan"
-    override fun getCheckovRunningCommandByServiceType(): ArrayList<String> {
+    private val volumeDirectory = FilenameUtils.separatorsToUnix(project.basePath)
+    private val volumeCertPath = "/usr/lib/ssl/cert.pem"
+    override fun getCheckovRunningCommandByServiceType(outputFilePath: String): ArrayList<String> {
         val pluginVersion =
                 PluginManagerCore.getPlugin(PluginId.getId("com.github.bridgecrewio.checkov"))?.version ?: "UNKNOWN"
 
@@ -20,11 +21,14 @@ class DockerCheckovScanCommandsService(project: Project) : CheckovScanCommandsSe
             dockerCommand.addAll(arrayListOf("--env", "PRISMA_API_URL=${prismaUrl}"))
         }
 
-        if (certPath?.isNotEmpty() == true) {
-            var volumeCaFile = "$certPath:/usr/lib/ssl/cert.pem"
-            dockerCommand.addAll(arrayListOf("--volume", volumeCaFile, "--env", "SSL_CERT_FILE=/usr/lib/ssl/cert.pem", "--env", "REQUESTS_CA_BUNDLE=/usr/lib/ssl/cert.pem"))
+        if (!certPath.isNullOrEmpty()) {
+            var volumeCaFile = "$certPath:$volumeCertPath"
+            dockerCommand.addAll(arrayListOf("--volume", volumeCaFile))
         }
-        val volumeDir = "${FilenameUtils.separatorsToUnix(project.basePath!!)}:/${volumeDirectory}"
+
+        dockerCommand.addAll(arrayListOf("--volume", "$outputFilePath:$outputFilePath"))
+
+        val volumeDir = "${FilenameUtils.separatorsToUnix(project.basePath)}:/${volumeDirectory}"
         dockerCommand.addAll(arrayListOf("--volume", volumeDir, image))
         return dockerCommand
 
@@ -36,5 +40,9 @@ class DockerCheckovScanCommandsService(project: Project) : CheckovScanCommandsSe
 
     override fun getFilePath(originalFilePath: String): String {
         return originalFilePath.replace(project.basePath!!, volumeDirectory)
+    }
+
+    override fun getCertPath(): String {
+        return volumeCertPath
     }
 }
--- src/main/kotlin/com/bridgecrew/ui/CheckovSettingsPanel.kt
+++ src/main/kotlin/com/bridgecrew/ui/CheckovSettingsPanel.kt
@@ -17,7 +17,7 @@ class CheckovSettingsPanel(project: Project): JPanel() {
 
         layout = GridLayoutManager(3, 1, Insets(0, 0, 0, 0), -1, -1)
 
-        add(JLabel("Checkov Plugin would scan your infrastructure as code files."), createGridRowCol(0,0, GridConstraints.ANCHOR_CENTER))
+        add(JLabel("Prisma Cloud Plugin would scan your infrastructure as code files."), createGridRowCol(0,0, GridConstraints.ANCHOR_CENTER))
         add(JLabel("Please configure a valid Prisma token in order to use this Plugin"), createGridRowCol(1,0, GridConstraints.ANCHOR_CENTER))
         val settingsButton = JButton("Open Settings")
 
